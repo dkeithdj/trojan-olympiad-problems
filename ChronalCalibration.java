ffa42338ace93890cb90ba46aa4333efda989d56
denreikeith <42316655+dkeithdj@users.noreply.github.com>
2024-03-14 04:06:46 +0800

    chronal calibration updates

--- ChronalCalibration.java
+++ ChronalCalibration.java
@@ -1,13 +1,15 @@
 import java.io.*;
 import java.util.*;
 
+// correct
 public class ChronalCalibration {
 
   public static void main(String[] args) throws FileNotFoundException {
 
     int frequency = 0;
 
-    Scanner infile = new Scanner(new FileReader("ChronalCalibration.in"));
+    // Scanner infile = new Scanner(new FileReader("ChronalCalibration.in"));
+    Scanner infile = new Scanner(System.in);
     while (infile.hasNext()) {
       frequency = frequency + infile.nextInt();
     }
