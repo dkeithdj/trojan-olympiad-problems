08d00ca9fab979ba55066df1db8faeecc0e70321
dkeithdj <42316655+dkeithdj@users.noreply.github.com>
2024-03-13 15:08:09 +0800

    rename

--- ChronalCalibration.java
+++ ChronalCalibration.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class ChronalCalibrationSolution {
+public class ChronalCalibration {
 
 	public static void main(String[] args) throws FileNotFoundException {
 
