ace445bb0a2d73469a853f604db7cd3e92b9855b
denreikeith <42316655+dkeithdj@users.noreply.github.com>
2024-03-14 04:44:44 +0800

    primepalindrome setup inputs & ans

--- PrimePalindrome.java
+++ PrimePalindrome.java
@@ -1,3 +1,11 @@
+import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class PrimePalindrome {
   public static int primePalindrome(int N) {
     for (int i = 1; i <= 5; i++) {
@@ -37,7 +45,46 @@ public class PrimePalindrome {
     return true;
   }
 
-  public static void main(String[] args) {
-    System.out.println(primePalindrome(6));
+  public static void main(String[] args) throws IOException {
+    Scanner sc = new Scanner(System.in);
+    // Scanner sc = new Scanner(new FileReader("PrimePalindrome.in"));
+
+    // BufferedWriter writer = new BufferedWriter(new
+    // FileWriter("PrimePalindrome.ans"));
+    while (sc.hasNextLine()) {
+      System.out.println(primePalindrome(Integer.parseInt(sc.nextLine())));
+      // writer.write(primePalindrome(Integer.parseInt(sc.nextLine())));
+    }
   }
 }
+
+// public class PrimePalindrome {
+// public static void main(String[] args) {
+// // ArrayList<Integer> numberList = generateRandomNumbers(20, 1, (int)
+// // Math.pow(10, 8));
+// ArrayList<Integer> numberList = generateRandomNumbers(10, 1000, 10000);
+
+// // Print the generated numbers per line
+// System.out.println("Generated Numbers:");
+// for (Integer number : numberList) {
+// System.out.println(number);
+// }
+// }
+
+// public static ArrayList<Integer> generateRandomNumbers(int count, int min,
+// int max) {
+// if (count <= 0 || max <= min) {
+// throw new IllegalArgumentException("Invalid parameters");
+// }
+
+// ArrayList<Integer> numbers = new ArrayList<>();
+// Random random = new Random();
+
+// for (int i = 0; i < count; i++) {
+// int randomNumber = random.nextInt((max - min) + 1) + min;
+// numbers.add(randomNumber);
+// }
+
+// return numbers;
+// }
+// }
