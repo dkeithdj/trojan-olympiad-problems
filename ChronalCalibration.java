48423ebdd31fab350693f33f18e4d5ad82afdf02
dkeithdj <42316655+dkeithdj@users.noreply.github.com>
2024-03-13 15:14:05 +0800

    spacing

--- ChronalCalibration.java
+++ ChronalCalibration.java
@@ -3,15 +3,15 @@ import java.util.*;
 
 public class ChronalCalibration {
 
-	public static void main(String[] args) throws FileNotFoundException {
+  public static void main(String[] args) throws FileNotFoundException {
 
-		int frequency = 0;
+    int frequency = 0;
 
-		Scanner infile = new Scanner(new FileReader("ChronalCalibration.in"));
-		while (infile.hasNext()) {
-			frequency = frequency + infile.nextInt();
-		}
+    Scanner infile = new Scanner(new FileReader("ChronalCalibration.in"));
+    while (infile.hasNext()) {
+      frequency = frequency + infile.nextInt();
+    }
 
-		System.out.println(frequency);
-	}
+    System.out.println(frequency);
+  }
 }
